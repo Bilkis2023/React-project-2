e1fa49a3ba8a73ed3745fb7a08e1f978b16383ec
Mst <bilkis.naher@yahoo.co.uk>
2025-01-02 23:53:51 +0000

    add all files for this project

---
+++ react-project-2/src/Card.jsx
@@ -0,0 +1,22 @@
+
+
+function Card() {
+    return (
+        <>
+        <div className="card" >
+            <img src="" alt=" " ></img>
+            <h1> Bilkis </h1>
+            <p> I make Youtube videos and play video games</p>
+        </div>
+        <hr/>
+         <div className="card" >
+         <img src="" alt=" " ></img>
+         <p> I make Youtube videos and play video games</p>
+         <p> </p>
+     </div>
+        
+     </>
+    )
+}
+
+export default Card
